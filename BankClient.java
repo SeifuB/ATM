ec6a737f5f8e1df7d9e5309d3bbf5f5af05477e4
Seifu Birega <50331075+SeifuB@users.noreply.github.com>
2021-02-24 20:11:02 +0300

    Update BankClient.java

--- BankClient.java
+++ BankClient.java
@@ -1,15 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package BankOperations;
-
 /* Admas University, School of Post Graduate Studies
 Department of Computer Science-Section2
 Course Name: Distributed Systems
 
  */
+package BankOperations;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.rmi.Naming;
